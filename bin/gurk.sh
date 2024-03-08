b40617c382aeedf121529b7604a92f2dcf6bca4f
David K <dkerrigan@mailbox.org>
2024-03-07 19:44:40 -0500

    auto commit

--- bin/gurk.sh
+++ bin/gurk.sh
@@ -3,5 +3,5 @@
 # Check if "gurk" is running
 if ! pgrep -x "gurk" > /dev/null; then
     # If not running, start "gurk"
-    gurk    # Replace with the actual command to start "gurk"
+    /home/david/Downloads/gurk    # Replace with the actual command to start "gurk"
 fi
