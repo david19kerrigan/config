f44b6cff3d57c19f3f3d50f7bb8da0b91ab0b567
David K <dkerrigan@mailbox.org>
2024-03-11 08:47:40 -0400

    auto commit

--- bin/light_mode.sh
+++ bin/light_mode.sh
@@ -3,3 +3,4 @@ sed -i 's/gtk-application-prefer-dark-theme=true/gtk-application-prefer-dark-the
 sed -i 's/include sol_dark.conf/include sol_light.conf/' /home/david/Documents/config/kitty/kitty.conf
 #sed -i 's/colorscheme catppuccin-macchiato/colorscheme catppuccin-latte/' /home/david/Documents/config/nvim/plugin/setup.vim
 sed -i 's/set background=dark/set background=light/' /home/david/Documents/config/nvim/plugin/setup.vim
+sed -i 's/set -g status-bg 0/set -g status-bg white/' /home/david/Documents/config/.tmux.conf
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/david/.local/share/omf/themes/ays/fish_prompt.fish
