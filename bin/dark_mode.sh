ea67716fa744bf98c917cca2a94299a7f63b54bf
David <david19kerrigan@gmail.com>
2024-06-21 13:55:45 -0400

    auto commit

--- bin/dark_mode.sh
+++ bin/dark_mode.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sed -i 's/gtk-application-prefer-dark-theme=false/gtk-application-prefer-dark-theme=true/' /home/david/Documents/config/gtk-3.0/settings.ini
+sed -i 's/gtk-application-prefer-dark-theme=false/gtk-application-prefer-dark-theme=true/' /home/david/Documents/config/gtk-4.0/settings.ini
 sed -i 's/include gruv_light.conf/include gruv_dark.conf/' /home/david/Documents/config/kitty/kitty.conf
 #sed -i 's/colorscheme catppuccin-latte/colorscheme catppuccin-macchiato/' /home/david/Documents/config/nvim/plugin/setup.vim
 sed -i 's/set background=light/set background=dark/' /home/david/Documents/config/nvim/plugin/setup.vim
--- bin/light_mode.sh
+++ bin/light_mode.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sed -i 's/gtk-application-prefer-dark-theme=true/gtk-application-prefer-dark-theme=false/' /home/david/Documents/config/gtk-3.0/settings.ini
+sed -i 's/gtk-application-prefer-dark-theme=true/gtk-application-prefer-dark-theme=false/' /home/david/Documents/config/gtk-4.0/settings.ini
 sed -i 's/include gruv_dark.conf/include gruv_light.conf/' /home/david/Documents/config/kitty/kitty.conf
 #sed -i 's/colorscheme catppuccin-macchiato/colorscheme catppuccin-latte/' /home/david/Documents/config/nvim/plugin/setup.vim
 sed -i 's/set background=dark/set background=light/' /home/david/Documents/config/nvim/plugin/setup.vim
