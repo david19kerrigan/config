fffd9bf1b391e7699fda447a0882176a98a2211b
David <david19kerrigan@gmail.com>
2024-06-25 19:02:03 -0400

    auto commit

--- bin/light_mode.sh
+++ bin/light_mode.sh
@@ -2,8 +2,8 @@
 sed -i 's/gtk-application-prefer-dark-theme=true/gtk-application-prefer-dark-theme=false/' /home/david/Documents/config/gtk-3.0/settings.ini
 sed -i 's/gtk-application-prefer-dark-theme=true/gtk-application-prefer-dark-theme=false/' /home/david/Documents/config/gtk-4.0/settings.ini
 sed -i 's/include gruv_dark.conf/include gruv_light.conf/' /home/david/Documents/config/kitty/kitty.conf
-#sed -i 's/colorscheme catppuccin-macchiato/colorscheme catppuccin-latte/' /home/david/Documents/config/nvim/plugin/setup.vim
 sed -i 's/set background=dark/set background=light/' /home/david/Documents/config/nvim/plugin/setup.vim
+sed -i 's/dark/light' /home/david/Documents/config/bat/config
 #sed -i 's/dark.jpg/light.jpg/' /home/david/.config/i3/config
 #feh --bg-scale /home/david/Documents/config/wallpapers/light.jpg
 cp /home/david/Documents/config/zathura/zathura-light /home/david/Documents/config/zathura/zathurarc
