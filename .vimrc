3149ebb2b8d63dfe18a41a5f5ae34fa0125d3837
David <david19kerrigan@gmail.com>
2024-06-25 19:48:47 -0400

    auto commit

--- .vimrc
+++ .vimrc
@@ -5,3 +5,5 @@ syntax on
 set rnu
 set number
 set clipboard=unnamedplus
+noremap <C-e> 5<C-e>
+noremap <C-y> 5<C-y>
--- fish/vars.fish
+++ fish/vars.fish
@@ -1,5 +1,5 @@
 set -U fish_user_paths /home/david/go/bin $fish_user_paths
 set -U fish_user_paths /home/david/.local/bin $fish_user_paths
 set -U fish_greeting
-set -U EDITOR nvim
-set -U VISUAL nvim
+set -U EDITOR vim
+set -U VISUAL vim
