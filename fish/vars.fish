51a793f8ab54c23713dcf534fdb89710886f3c19
David <david19kerrigan@gmail.com>
2024-06-28 12:55:35 -0400

    auto commit

--- fish/vars.fish
+++ fish/vars.fish
@@ -2,5 +2,3 @@ set -U fish_user_paths /home/david/go/bin $fish_user_paths
 set -U fish_user_paths /home/david/.local/bin $fish_user_paths
 set -U fish_user_paths /home/david/.config/emacs/bin $fish_user_paths
 set -U fish_greeting
-set -U EDITOR edit
-set -U VISUAL edit
