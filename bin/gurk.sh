15dcbedbcbe23424762905a029566d730b07bf68
David <david19kerrigan@gmail.com>
2024-10-16 19:42:38 -0400

    auto commit

--- bin/gurk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Check if "gurk" is running
-if ! pgrep -x "gurk" > /dev/null; then
-    # If not running, start "gurk"
-    /home/david/Downloads/gurk    # Replace with the actual command to start "gurk"
-fi
