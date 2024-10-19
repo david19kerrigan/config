3c31f66305025b3414367c1afe34f61e8186f9c7
David Kerrigan <david19kerrigan@gmail.com>
2024-10-19 12:46:43 -0400

    auto commit

--- link.sh
+++ link.sh
@@ -12,8 +12,7 @@ ln -s ~/Documents/config/.gnupg ~/
 ln -s ~/Documents/config/.mpd ~/
 ln -s ~/Documents/config/.ncmpcpp ~/
 ln -s ~/Documents/config/bin ~/.local/
-ln -s ~/Documents/config/mutt ~/.config/
-ln -s ~/Documents/config/msmtp ~/.config/
 ln -s ~/Documents/config/mpv ~/.config/
+ln -s ~/Documents/config/greg ~/.config/
 ln -s ~/Documents/config/gtk-3.0 ~/.config/
 ln -s ~/Documents/config/gtk-4.0 ~/.config/
