a6b590ba2c8c1829ee85b96314c2284ed78d687f
David <david19kerrigan@gmail.com>
2024-10-21 12:03:38 -0400

    auto commit

--- bin/t480.sh
+++ bin/t480.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if DP-1 is connected and active
-if xrandr | grep -q "DP-1 connected"; then
+if xrandr | grep -q "^DP-1 connected"; then
     # Disable eDP-1
     xrandr --output eDP-1 --off
 else
