a98edd1c59e40f95af7b648855fbde619cfd61e3
David <david19kerrigan@gmail.com>
2024-11-06 07:09:26 -0500

    auto commit

--- bin/t480.sh
+++ bin/t480.sh
@@ -4,7 +4,10 @@ if xrandr | grep -q "^DP-1 connected"; then
     xrandr --output eDP-1 --off
 fi
 
+if ! lsusb | grep -q "04fe:0020 "; then
+    setxkbmap -layout us -option ctrl:swapcaps
+fi
+
 xinput set-prop "TPPS/2 IBM TrackPoint" "Coordinate Transformation Matrix" 20 0 0 0 20 0 0 0 1
 xinput set-prop "TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
 brightnessctl set 100%
-setxkbmap -layout us -option ctrl:swapcaps
