a76b09b19e1311e88ed28262811742632607ef52
David <david19kerrigan@gmail.com>
2024-11-10 19:08:32 -0500

    auto commit

--- link.sh
+++ link.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 mkdir ~/.config/gtk-3.0
 mkdir ~/.config/gtk-4.0
-mkdir ~/.config/lf
 mkdir ~/.moc
 ln -s ~/Documents/config/dunst ~/.config/
 ln -s ~/Documents/config/mksh/.mkshrc ~/
@@ -9,19 +8,18 @@ ln -s ~/Documents/config/mksh/.profile ~/
 ln -s ~/Documents/config/mpv ~/.config/
 ln -s ~/Documents/config/zathura ~/.config/
 ln -s ~/Documents/config/vis ~/.config/
-ln -s ~/Documents/config/.tmux.conf ~/
 ln -s ~/Documents/config/picom.conf ~/.config/
 ln -s ~/Documents/config/.xinitrc ~/
 ln -s ~/Documents/config/.xbindkeysrc ~/
 ln -s ~/Documents/config/.gnupg/gpg-agent.conf ~/.gnupg/
-ln -s ~/Documents/config/.mpd ~/
-ln -s ~/Documents/config/.ncmpcpp ~/
 ln -s ~/Documents/config/bin ~/.local/
 ln -s ~/Documents/config/mpv ~/.config/
-ln -s ~/Documents/config/greg ~/.config/
 ln -s ~/Documents/config/newsraft ~/.config/
 ln -s ~/Documents/config/settings.ini ~/.config/gtk-3.0/
 ln -s ~/Documents/config/settings.ini ~/.config/gtk-4.0/
 ln -s ~/Documents/config/mimeapps.list ~/.config/
 ln -s ~/Documents/config/lf ~/.config/
-ln -s ~/Documents/config/moc/* ~/.moc/
+ln -s ~/Documents/config/aerc ~/.config/
+ln -s ~/Documents/config/.moc/config ~/.moc/
+ln -s ~/Documents/config/.moc/keymap ~/.moc/
+ln -s ~/Documents/config/.mbsyncrc ~/
