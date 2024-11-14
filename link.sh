f3dcbf1b5873ed6217093db8f6f5f85a58a4f27a
David <david19kerrigan@gmail.com>
2024-11-13 21:23:57 -0500

    auto commit

--- link.sh
+++ link.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir ~/.config/gtk-3.0
 mkdir ~/.config/gtk-4.0
-mkdir ~/.moc
+mkdir ~/.sfeed
+mkdir ~/.mpd
 ln -s ~/Documents/config/dunst ~/.config/
 ln -s ~/Documents/config/mksh/.mkshrc ~/
 ln -s ~/Documents/config/mksh/.profile ~/
@@ -14,12 +15,10 @@ ln -s ~/Documents/config/.xbindkeysrc ~/
 ln -s ~/Documents/config/.gnupg/gpg-agent.conf ~/.gnupg/
 ln -s ~/Documents/config/bin ~/.local/
 ln -s ~/Documents/config/mpv ~/.config/
-ln -s ~/Documents/config/newsraft ~/.config/
 ln -s ~/Documents/config/settings.ini ~/.config/gtk-3.0/
 ln -s ~/Documents/config/settings.ini ~/.config/gtk-4.0/
 ln -s ~/Documents/config/mimeapps.list ~/.config/
-ln -s ~/Documents/config/lf ~/.config/
 ln -s ~/Documents/config/aerc ~/.config/
-ln -s ~/Documents/config/.moc/config ~/.moc/
-ln -s ~/Documents/config/.moc/keymap ~/.moc/
 ln -s ~/Documents/config/.mbsyncrc ~/
+ln -s ~/Documents/config/.sfeed/sfeedrc ~/.sfeed/
+ln -s ~/Documents/config/.mpd/mpd.conf ~/.mpd/
