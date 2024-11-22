67f7695d543f93b842fed7fcbda0e740b3a2649e
David <david19kerrigan@gmail.com>
2024-11-22 07:48:30 -0500

    update linking

--- link.sh
+++ link.sh
@@ -4,22 +4,22 @@ mkdir $HOME/config/gtk-4.0
 mkdir $HOME/.sfeed
 mkdir $HOME/.config/mpd
 mkdir $HOME/.mblaze
-ln -sf ./mksh/.mkshrc $HOME/
-ln -sf ./mksh/.profile $HOME/
-ln -sf ./mpv $XDG_CONFIG_HOME/
-ln -sf ./zathura $XDG_CONFIG_HOME/
-ln -sf ./vis $XDG_CONFIG_HOME/
-ln -sf ./picom.conf $XDG_CONFIG_HOME/
-ln -sf ./.xinitrc $HOME/
-ln -sf ./.xbindkeysrc $HOME/
-ln -sf ./.gnupg/gpg-agent.conf $HOME/.gnupg/
-ln -sf ./bin $HOME/.local/
-ln -sf ./mpv $XDG_CONFIG_HOME/
-ln -sf ./settings.ini $XDG_CONFIG_HOME/gtk-3.0/
-ln -sf ./settings.ini $XDG_CONFIG_HOME/gtk-4.0/
-ln -sf ./mimeapps.list $XDG_CONFIG_HOME/
-ln -sf ./.mbsyncrc $HOME/
-ln -sf ./.sfeed/sfeedrc $HOME/.sfeed/
-ln -sf ./mpd/mpd.conf $XDG_CONFIG_HOME/mpd/
-ln -sf ./.msmtprc $XDG_CONFIG_HOME/msmtp/config
-ln -sf ./lesskey $HOME
+ln -sf $PWD/mksh/.mkshrc $HOME/
+ln -sf $PWD/mksh/.profile $HOME/
+ln -sf $PWD/mpv $XDG_CONFIG_HOME/
+ln -sf $PWD/zathura $XDG_CONFIG_HOME/
+ln -sf $PWD/vis $XDG_CONFIG_HOME/
+ln -sf $PWD/picom.conf $XDG_CONFIG_HOME/
+ln -sf $PWD/.xinitrc $HOME/
+ln -sf $PWD/.xbindkeysrc $HOME/
+ln -sf $PWD/.gnupg/gpg-agent.conf $HOME/.gnupg/
+ln -sf $PWD/bin $HOME/.local/
+ln -sf $PWD/mpv $XDG_CONFIG_HOME/
+ln -sf $PWD/settings.ini $XDG_CONFIG_HOME/gtk-3.0/
+ln -sf $PWD/settings.ini $XDG_CONFIG_HOME/gtk-4.0/
+ln -sf $PWD/mimeapps.list $XDG_CONFIG_HOME/
+ln -sf $PWD/.mbsyncrc $HOME/
+ln -sf $PWD/.sfeed/sfeedrc $HOME/.sfeed/
+ln -sf $PWD/mpd/mpd.conf $XDG_CONFIG_HOME/mpd/
+ln -sf $PWD/.msmtprc $XDG_CONFIG_HOME/msmtp/config
+ln -sf $PWD/lesskey $XDG_CONFIG_HOME/
--- mksh/.profile
+++ mksh/.profile
@@ -3,6 +3,7 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority 
 export PASSWORD_STORE_DIR="$XDG_DATA_HOME"/pass 
+export PATH=$PATH:$HOME/.local/bin
 
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
     startx
