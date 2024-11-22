21162379f8528f2789a4d488e6dd7e01f9eade44
David <david19kerrigan@gmail.com>
2024-11-22 05:18:12 -0500

    update linking

--- link.sh
+++ link.sh
@@ -4,22 +4,22 @@ mkdir $HOME/config/gtk-4.0
 mkdir $HOME/.sfeed
 mkdir $HOME/.config/mpd
 mkdir $HOME/.mblaze
-ln -sf $HOME/dox/config/mksh/.mkshrc $HOME/
-ln -sf $HOME/dox/config/mksh/.profile $HOME/
-ln -sf $HOME/dox/config/mpv $XDG_CONFIG_HOME/
-ln -sf $HOME/dox/config/zathura $XDG_CONFIG_HOME/
-ln -sf $HOME/dox/config/vis $XDG_CONFIG_HOME/
-ln -sf $HOME/dox/config/picom.conf $XDG_CONFIG_HOME/
-ln -sf $HOME/dox/config/.xinitrc $HOME/
-ln -sf $HOME/dox/config/.xbindkeysrc $HOME/
-ln -sf $HOME/dox/config/.gnupg/gpg-agent.conf $HOME/.gnupg/
-ln -sf $HOME/dox/config/bin $HOME/.local/
-ln -sf $HOME/dox/config/mpv $XDG_CONFIG_HOME/
-ln -sf $HOME/dox/config/settings.ini $XDG_CONFIG_HOME/gtk-3.0/
-ln -sf $HOME/dox/config/settings.ini $XDG_CONFIG_HOME/gtk-4.0/
-ln -sf $HOME/dox/config/mimeapps.list $XDG_CONFIG_HOME/
-ln -sf $HOME/dox/config/.mbsyncrc $HOME/
-ln -sf $HOME/dox/config/.sfeed/sfeedrc $HOME/.sfeed/
-ln -sf $HOME/dox/config/mpd/mpd.conf $XDG_CONFIG_HOME/mpd/
-ln -sf $HOME/dox/config/.msmtprc $XDG_CONFIG_HOME/msmtp/config
-ln -sf $HOME/dox/config/lesskey $HOME
+ln -sf ./mksh/.mkshrc $HOME/
+ln -sf ./mksh/.profile $HOME/
+ln -sf ./mpv $XDG_CONFIG_HOME/
+ln -sf ./zathura $XDG_CONFIG_HOME/
+ln -sf ./vis $XDG_CONFIG_HOME/
+ln -sf ./picom.conf $XDG_CONFIG_HOME/
+ln -sf ./.xinitrc $HOME/
+ln -sf ./.xbindkeysrc $HOME/
+ln -sf ./.gnupg/gpg-agent.conf $HOME/.gnupg/
+ln -sf ./bin $HOME/.local/
+ln -sf ./mpv $XDG_CONFIG_HOME/
+ln -sf ./settings.ini $XDG_CONFIG_HOME/gtk-3.0/
+ln -sf ./settings.ini $XDG_CONFIG_HOME/gtk-4.0/
+ln -sf ./mimeapps.list $XDG_CONFIG_HOME/
+ln -sf ./.mbsyncrc $HOME/
+ln -sf ./.sfeed/sfeedrc $HOME/.sfeed/
+ln -sf ./mpd/mpd.conf $XDG_CONFIG_HOME/mpd/
+ln -sf ./.msmtprc $XDG_CONFIG_HOME/msmtp/config
+ln -sf ./lesskey $HOME
--- mksh/.profile
+++ mksh/.profile
@@ -1,4 +1,8 @@
 export ENV="/home/david/.mkshrc"
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_HOME=$HOME/.local/share
+export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority 
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME"/pass 
 
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
     startx
