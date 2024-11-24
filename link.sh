3ecc0cc89e6a1b630d5d8b2f3e47120844f8b65e
David <david19kerrigan@gmail.com>
2024-11-23 21:20:37 -0500

    update mkshrc

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir $HOME/.config/gtk-3.0
-mkdir $HOME/config/gtk-4.0
+mkdir $HOME/.config/gtk-4.0
 mkdir $HOME/.sfeed
 mkdir $HOME/.config/mpd
 mkdir $HOME/.mblaze
@@ -12,7 +12,7 @@ ln -sf $PWD/vis $XDG_CONFIG_HOME/
 ln -sf $PWD/picom.conf $XDG_CONFIG_HOME/
 ln -sf $PWD/.xinitrc $HOME/
 ln -sf $PWD/.xbindkeysrc $HOME/
-ln -sf $PWD/.gnupg/gpg-agent.conf $HOME/.gnupg/
+ln -sf $PWD/gnupg/gpg-agent.conf $XDG_DATA_HOME/gnupg
 ln -sf $PWD/bin $HOME/.local/
 ln -sf $PWD/mpv $XDG_CONFIG_HOME/
 ln -sf $PWD/settings.ini $XDG_CONFIG_HOME/gtk-3.0/
@@ -21,5 +21,5 @@ ln -sf $PWD/mimeapps.list $XDG_CONFIG_HOME/
 ln -sf $PWD/.mbsyncrc $HOME/
 ln -sf $PWD/.sfeed/sfeedrc $HOME/.sfeed/
 ln -sf $PWD/mpd/mpd.conf $XDG_CONFIG_HOME/mpd/
-ln -sf $PWD/.msmtprc $XDG_CONFIG_HOME/msmtp/config
+ln -sf $PWD/msmtp $XDG_CONFIG_HOME/
 ln -sf $PWD/lesskey $XDG_CONFIG_HOME/
